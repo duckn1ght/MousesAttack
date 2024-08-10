c0ee4625d5cf0a9ce7a9ea64a12363a091e4cad0
duckn1ght <duckn1ght04@gmail.com>
2024-08-10 23:41:50 +0500

    start scene

---
+++ scripts/start_scene.js
@@ -0,0 +1,27 @@
+const image = document.getElementById("image");
+
+function wait(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+async function run() {
+  image.src = "./assets/start_scene/1.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/2.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/3.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/4.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/5.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/6.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/7.png";
+  await wait(1000);
+  image.src = "./assets/start_scene/8.png";
+  await wait(1000);
+  window.location = "../game.html";
+}
+
+run();
